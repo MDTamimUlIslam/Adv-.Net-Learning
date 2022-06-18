88a3d838c27a088be554fdf79b28c052b27df5c7
MDTamimUlIslam <tamimullislam2012@gmail.com>
2022-06-18 13:06:39 +0600

    Form validation work added

---
+++ Form Validation/Form Validation/Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using Form_Validation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Form_Validation.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new student());
+        }
+        [HttpPost]
+        public ActionResult Create(student s)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Dash");
+            }
+            else
+            {
+                return View(s);
+            }
+
+        }
+       // [HttpPost]
+        public ActionResult Dash()
+        {
+            return View();
+        }
+    }
+}
---
+++ Form Validation/Form Validation/Models/student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Form_Validation.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Form_Validation.Models
+{
+    public class student
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "Please Insert a Name")]
+        public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^\d{2}-\d{5}-\d{1}$", ErrorMessage = "Input Aiub Id format-- XX-XXXXX-X")]
+        public string Id { get; set; }
+        [Required]
+        public string Dob { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
