a5fca5eac413fba0a23ba74e0a661154854f7280
MDTamimUlIslam <tamimullislam2012@gmail.com>
2022-06-29 11:00:25 +0600

    Entity framework

---
+++ AdminReg/AdminReg/Controllers/AdminListController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AdminReg.Models.Database;
+using System.Web.Mvc;
+
+namespace AdminReg.Controllers
+{
+    public class AdminListController : Controller
+    {
+        // GET: AdminList
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ShowAdmin()
+        {
+            Summer_TEntities db = new Summer_TEntities();
+            var data = db.AdminLists.ToList();
+            return View(data);
+
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+
+            return View(new AdminList());
+        }
+        [HttpPost]
+        public ActionResult Create(AdminList b)
+        {
+            if (ModelState.IsValid)
+            {
+                Summer_TEntities db = new Summer_TEntities();
+                db.AdminLists.Add(b);
+                db.SaveChanges();
+                return RedirectToAction("ShowAdmin");
+            }
+            return View();
+        }
+        
+       public ActionResult Edit(AdminList sub_b)
+        {
+            Summer_TEntities db = new Summer_TEntities();
+            var adminlist = (from b in db.AdminLists
+                        where b.Id == sub_b.Id
+                        select b).FirstOrDefault();
+            db.Entry(adminlist).CurrentValues.SetValues(sub_b);
+            db.SaveChanges();
+            return RedirectToAction("ShowAdmin");
+        }
+    }
+}
---
+++ EF/EF/Controllers/BookController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EF.Models.Database;
+using System.Web.Mvc;
+
+
+namespace EF.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            Summer_TEntities db = new Summer_TEntities();
+            var data = db.Books.ToList();
+            return View(data);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new Book());
+        }
+        [HttpPost]
+        public ActionResult Create(Book b)
+        {
+            if (ModelState.IsValid)
+            {
+                Summer_TEntities db = new Summer_TEntities();
+                db.Books.Add(b);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    
+        public ActionResult Edit(int Id)
+        {
+            Summer_TEntities db = new Summer_TEntities();
+            var book = (from b in db.Books
+                        where b.Id == Id
+                        select b).FirstOrDefault();  
+
+            return View(book);
+        }
+        [HttpPost]
+        public ActionResult Edit(Book sub_b)
+        {
+            Summer_TEntities db = new Summer_TEntities();
+            var book =(from b in db.Books
+                       where b.Id == sub_b.Id
+                       select b).FirstOrDefault();
+            db.Entry(book).CurrentValues.SetValues(sub_b);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
